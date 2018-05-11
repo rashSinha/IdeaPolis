ac115522a399d28a3cfac27a440b96bc140f5778
DESKTOP-U1F4GIN\rashmil <rashmil.sinha22@gmail.com>
2018-05-11 13:30:46 -0700

    second commit

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production') {
-  module.exports = {mongoURI: 'mongodb://<rashmil>:<rash>@ds119800.mlab.com:19800/vidjot-prod'}
+  module.exports = {mongoURI: 'mongodb://rashmil:rash@ds119800.mlab.com:19800/vidjot-prod'}
 } else {
   module.exports = {mongoURI: 'mongodb://localhost/vidjot-dev'}
 }
